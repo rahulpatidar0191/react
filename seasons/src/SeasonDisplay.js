48958953363405f7c94b1911b0865421692ace5e
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2020-04-20 09:11:06 -0700

    component life cycle

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import './SeasonDisplay.css'
+import React from "react";
 
-const SeasonDisplay = () => {
-   return (
-       <div> Hi there </div>
-   )
+const seasonConfig = {
+    summer: {
+    text : "Let's hit the beach bitch",
+    iconName : "sun"
+    },
+    winter :{
+        text : "Burr, It is chilly",
+        iconName: "snowflake"
+
+    }
 }
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter"; //lat>0 northen hemisphere
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season]
+  return (
+    <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon right`} />
+        <h1> { text }</h1>
+        <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,65 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDom from "react-dom";
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-class App extends React.Component {
-  constructor(props) {
-    //better to intialize state here as it needs to be define before the componenet and constructor willl be called first
-    super(props); //to make sure the code inside React.Componenet still gets called as we overweite it with using constructor , super is a refrence to parents constructor
+class App extends Component {
 
-    //THIS IS THE only time we do direct assigment to this.state
-    this.state = { lat: null, errorMessage: "" };
+  state = {lat: null, errorMessage: ''}
 
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //WE called seState to update value of state
-        this.setState({ lat: position.coords.latitude }); //setState is predefine by REact.Componenet
-      },
-      (err) => {
-        console.log(err);
-        this.setState({ errorMessage: err.message });
-      }
-    );
+      position=> this.setState({ lat: position.coords.latitude }),     //WE called seState to update value of state //setState is predefine by REact.Componenet
+      err => this.setState({ errorMessage: err.message })
+    )
+
   }
-  //React says we have t define a render!!
-  render() {
+  //React says we have t define a render!!  always returns jsx
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error : {this.state.errorMessage} </div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-        return <div> latitude : {this.state.lat} </div>;
-      }
-      return <div> Loading </div>;
-
+      return <SeasonDisplay lat={this.state.lat}/>;
+    }
+    return <Spinner  />
   }
+
+  render(){
+    return(
+      <div className= "border-red">
+      {this.renderContent()}
+      </div>
+    )
+  }
+
 }
 
 ReactDom.render(<App />, document.querySelector("#root"));
 
 
+//componentDidMount : Same as Constructor, Use to load data once, MAny people say do not do data loading in a constructor, recommded to do here 
+//componentDidUpdate : Place to do more data loading when state/prop changes 
+//componentWillUnmount : Goodd place to do cleanup (especially for non react stuff), used rarley 
+
+
+
+
+  // constructor(props) {
+  //   //better to intialize state here as it needs to be define before the componenet and constructor willl be called first
+  //   super(props); //to make sure the code inside React.Componenet still gets called as we overweite it with using constructor , super is a refrence to parents constructor
+
+  //   //THIS IS THE only time we do direct assigment to this.state
+  //   this.state = { lat: null, errorMessage: "" };
+
+  //   window.navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       //WE called seState to update value of state
+  //       this.setState({ lat: position.coords.latitude }); //setState is predefine by REact.Componenet
+  //     },
+  //     (err) => {
+  //       console.log(err);
+  //       this.setState({ errorMessage: err.message });
+  //     }
+  //   )
+  // }
